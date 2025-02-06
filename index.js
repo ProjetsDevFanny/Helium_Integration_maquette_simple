92b8d66cf44fbd9b46bcaa9063b6257363a840a9
Fanny Simon <contact.fanny.simon@gmail.com>
2025-02-06 12:15:56 +0100

    Rendre les liens cliquables

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
 
 // Ajouter un évènement sur la fenêtre (window) puis animer la position de ces ronds (top, left injecter "e")
 
-// Quand on passe sur les liens ils deviennent "clicables" = plus gros en z-index +1
+// Donner un style de transparence aux ronds (mix-blend-mode)
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+// Quand on passe sur les liens ils deviennent "clicables" = plus gros en z-index +1
 
 // --------------------------------------------------------------------------------------------
 
+// On va chercher les cercles pour faire notre evenement "mousemove" dessus :
 const mousemoveSmall = document.querySelector(".mousemove_small");
 const mousemoveMiddle = document.querySelector(".mousemove_middle");
 const mousemoveLarge = document.querySelector(".mousemove_large");
 // console.log(mousemoveSmall);
 // console.log(mousemoveMiddle);
 // console.log(mousemoveLarge);
+
+// On va chercher les liens de navigations pour les rendre cliquables:
+const helium = document.querySelector("h3");
+const navLink = document.querySelector("ul");
 
 // On va chercher des data sur l'event "mousemove"
 // window.addEventListener ('mousemove', (e) => {
@@ -23,14 +28,20 @@ const mousemoveLarge = document.querySelector(".mousemove_large");
 window.addEventListener("mousemove", (e) => {
   mousemoveSmall.style.left = e.pageX + "px";
   mousemoveSmall.style.top = e.pageY + "px";
-});
-
-window.addEventListener("mousemove", (e) => {
   mousemoveMiddle.style.left = e.pageX + "px";
   mousemoveMiddle.style.top = e.pageY + "px";
+  mousemoveLarge.style.left = e.pageX + "px";
+  mousemoveLarge.style.top = e.pageY + "px";
 });
 
-window.addEventListener("mousemove", (e) => {
- mousemoveLarge.style.left = e.pageX + "px";
- mousemoveLarge.style.top = e.pageY + "px";
-});
+// Liens cliquables:
+// helium.addEventListener("mouseover", (event) => {
+//   event.target.style.color = "blue";
+
+//  });
+// helium.addEventListener("mouseout", (event) => {
+//   event.target.style.color = "white";
+
+//  });
+
+
